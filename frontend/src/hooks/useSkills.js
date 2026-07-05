ca28943c91a43fcfe73d60eff401c64e838b3831
Suraj Bhavake <surajbhavake2@gmail.com>
2026-07-05 21:52:15 +0530

    chore : finish useSkills using tanStack for query

---
+++ frontend/src/hooks/useSkills.js
@@ -0,0 +1,39 @@
+import { useQuery,useMutation,useQueryClient } from "@tanstack/react-query";
+import api from "../api/axios";
+
+export const useSkill = () =>{
+    return useQuery({
+        queryKey : ['skills'],
+        queryFn : async() =>{
+            const {data} = await api.get('/skills/');
+            return data;
+        }
+    })
+}
+
+export const useCreateSkill = () =>{
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn : (newSkill) => api.post('/skills/',newSkill),
+        onSuccess : () => {queryClient.invalidateQueries({queryKey : ['skills']})}
+    })
+}
+
+export const useUpdateSkill = () =>{
+    const queryClient = useQueryClient()
+    return useMutation({
+        mutationFn : (updatedSkills) => {
+            const {id,...payload} = updatedSkills;
+            return api.patch(`/skills/${id}/`,updatedSkills)
+        },
+        onSuccess : ()=>queryClient.invalidateQueries({queryKey:['skills']})
+    })
+}
+
+export const useDeleteSkill = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn:(id)=> api.delete(`/skills/${id}/`),
+        onSuccess : ()=>queryClient.invalidateQueries({queryKey:['skills']})
+    })
+}
