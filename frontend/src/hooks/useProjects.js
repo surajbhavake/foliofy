82b4961dcdca3ef3d15f844e1b67ea2013b11b3d
Suraj Bhavake <surajbhavake2@gmail.com>
2026-07-05 21:32:46 +0530

    chore : finish useProject using tanStack for query

---
+++ frontend/src/hooks/useProjects.js
@@ -0,0 +1,58 @@
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import api from '../api/axios';
+
+export const useProject = () =>{
+    return useQuery({
+        queryKey : ['projects'],
+        queryFn : async()=>{
+            const {data} = await api.get('/projects/');
+            return data;
+        }
+    })
+}
+
+
+export const useCreateProject = () =>{
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn : (formData) => api.post('/projects/',formData,{
+            headers : {
+                'Content-Type': 'multipart/form-data'
+            },
+        }),
+        onSuccess : () =>{
+            queryClient.invalidateQueries({
+                queryKey : ['projects']
+            })
+        }
+    })
+}
+
+
+export const useUpdateProject = () =>{
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn :(formData) =>{
+            const {id,...payload} = formData;
+            return api.patch(`/project/${id}/`,formData,{
+                headers : {
+                    'Content-Type' : 'multipart/form-data'
+                },
+            })
+        },
+        onSuccess : () =>{
+            queryClient.invalidateQueries({
+                queryKey : ['projects']
+            })
+        }
+
+    })
+}
+
+export const useDeleteProject = () =>{
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn : (id) => api.delete(`/projects/${id}/`),
+        onSuccess : () => queryClient.invalidateQueries({queryKey:['projects']})
+    })
+}
