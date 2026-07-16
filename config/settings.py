cf2953223f08331439015469d44d3a6ee228ff32
Suraj Bhavake <surajbhavake2@gmail.com>
2026-07-16 16:02:28 +0530

    chore : working on deployment

--- config/settings.py
+++ config/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xqp*pnj)f=hdcva9r89+sqmkg)o3^ot9wlh(#238)10j5hbk0f'
-
+# SECRET_KEY = 'django-insecure-xqp*pnj)f=hdcva9r89+sqmkg)o3^ot9wlh(#238)10j5hbk0f'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-fallback-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =  os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
