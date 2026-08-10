7b85924fde1fdcb2aad63065424a082182c17e2d
surajbhavake <surajbhavake2@gmail.com>
2026-08-10 22:27:09 +0530

    chore: added experience and contact models in project

--- core/models.py
+++ core/models.py
@@ -25,6 +25,13 @@ class Profile(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
 
+    #contact
+    contact_email = models.EmailField(blank=True,help_text='Email where you to receive messages')
+    show_contact_form = models.BooleanField(default=True,help_text='Display the contact form on your porfolio ')
+    location = models.CharField(max_length=200,blank=True)
+    available_for = models.CharField(max_length=300,blank=True,help_text='eg. freelance projects, full-time roles')
+
+
     def __str__(self):
         return self.full_name
     
@@ -87,3 +94,53 @@ def create_user_profile(sender,instance,created,**kwargs):
                 'theme' : 'default',
             }
         )
+
+
+class Experience(models.Model):
+
+    profile = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name='experiences'#when i write profile.experiences.all() it will show me all the experiences that profile has so this makes the process easy
+
+        )
+
+    company = models.CharField(max_length=200)
+    role = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    start_date = models.DateField()
+    end_date = models.DateField(blank=True,null=True)
+    is_current = models.BooleanField(default=False)
+    order = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['order','-start_date']
+        verbose_name_plural = 'Experiences'
+
+    def __str__(self):
+        return f"{self.role} at {self.company}"
+
+
+
+class ContactMessage(models.Model):
+
+    profile = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name='contact_message'
+    )
+
+    sender_name = models.CharField(max_length=200)
+    sender_email = models.EmailField()
+    subject = models.CharField(max_length=300)
+    message = models.TextField()
+    is_read = models.BooleanField(default=False)
+    created_at= models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"Message from {self.sender_name}:{self.subject[:50]}"
