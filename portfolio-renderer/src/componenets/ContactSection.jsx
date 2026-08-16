dc729d688df58d3550c1bd455a29e574aa58ad84
surajbhavake <surajbhavake2@gmail.com>
2026-08-17 00:52:10 +0530

    chore: add experience hook to the project updated

--- portfolio-renderer/src/componenets/ContactSection.jsx
+++ portfolio-renderer/src/componenets/ContactSection.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
-import api from '../api/axios';
+import api from '../api/axiox';
 import { FiSend, FiMapPin, FiMail } from 'react-icons/fi';
 
 const contactSchema = z.object({
